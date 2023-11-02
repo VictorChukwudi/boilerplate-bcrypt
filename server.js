8bd13999335abd4ebd1a87061ab7a3471d18e702
VictorChukwudi <victorukay@gmail.com>
2023-11-02 14:04:47 +0100

    async-bcrypt

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, 13, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
 
 //END_ASYNC
 
@@ -27,7 +33,7 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 
-
+// https://infosec-bcrypt-asjs.onrender.com
 
 
 
